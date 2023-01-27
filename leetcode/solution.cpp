fc85801e370420d4d1c2e8e6d0220f7350525ee6
Lihong Ji <294672570@qq.com>
2023-01-27 04:05:47 +0100

    leetcode 3

--- leetcode/solution.cpp
+++ leetcode/solution.cpp
@@ -10,10 +10,10 @@ public:
 
     int lengthOfLongestSubstring() {
         vector<int> pos(128, -1);
-        int ans = 0;
-        for (int i=0, j=0; i < s.length(); i++){
-            j = max(j, pos[s[i]] + 1);  // s[i]
-            ans = max(ans, i - j + 1);
+        int ans = 0; 
+        for (int i=0, j=0; i < s.length(); i++){    // i,j -> right,left bound
+            j = max(j, pos[s[i]] + 1);  // s[i] --> ASCII（0->127） code to record the letter position in the given s.
+            ans = max(ans, i - j + 1);  
             pos[s[i]] = i;
         }
         return ans;
